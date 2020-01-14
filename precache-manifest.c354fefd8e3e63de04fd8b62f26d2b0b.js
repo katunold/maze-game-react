478a850d6de201a7d0369e67346ad60beafa9980
katunold <katunold94@gmail.com>
2020-01-14 15:38:34 +0300

    Updates

---
+++ precache-manifest.c354fefd8e3e63de04fd8b62f26d2b0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5720c4f11164704490c17fc7719dbc",
+    "url": "/maze-game-react/index.html"
+  },
+  {
+    "revision": "4c170f0da2170c950b67",
+    "url": "/maze-game-react/static/css/main.45c55309.chunk.css"
+  },
+  {
+    "revision": "929931201cf1e5d76051",
+    "url": "/maze-game-react/static/js/2.3300653c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/maze-game-react/static/js/2.3300653c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4c170f0da2170c950b67",
+    "url": "/maze-game-react/static/js/main.bb92b9e6.chunk.js"
+  },
+  {
+    "revision": "948c5116cb8f4e85c45a",
+    "url": "/maze-game-react/static/js/runtime-main.288e268a.js"
+  }
+]);
